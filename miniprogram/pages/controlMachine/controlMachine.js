d95aff6b105cf3d0a8fef27e40a96091e263ab49
Zhang Maiyun <myzhang1029@hotmail.com>
2020-09-18 22:32:40 +0800

    Something about controlMachine

--- miniprogram/pages/controlMachine/controlMachine.js
+++ miniprogram/pages/controlMachine/controlMachine.js
@@ -8,19 +8,73 @@ Page({
     title: "Machine Control",
     subtitle: "",
     showAfterMidnightNotice: false,
-    buttons: [
+    /* TODO: Find real location for the images */
+    machines: [
+      /* Boy side */
       {
-        type: "default",
-        className: "",
-        text: "I'll use anyway",
-        value: 0
+        name: "w1",
+        location: ["10rpx", "2rpx"],
+        handlerName: "w1Click"
       },
       {
-        type: "primary",
-        className: "",
-        text: "Sure, I see",
-        value: 1
+        name: "w2",
+        location: ["12rpx", "2rpx"],
+        handlerName: "w2Click"
+      },
+      {
+        name: "d1",
+        location: ["15rpx", "2rpx"],
+        handlerName: "d1Click"
+      },
+      {
+        name: "d2",
+        location: ["1rpx", "2rpx"],
+        handlerName: "d2Click"
+      },
+      {
+        name: "w3",
+        location: ["13rpx", "2rpx"],
+        handlerName: "w3Click"
+      },
+      /* Girl side */
+      {
+        name: "w4",
+        location: ["19rpx", "2rpx"],
+        handlerName: "w4Click"
+      },
+      {
+        name: "w5",
+        location: ["24rpx", "2rpx"],
+        handlerName: "w5Click"
+      },
+      {
+        name: "d3",
+        location: ["45rpx", "2rpx"],
+        handlerName: "d3Click"
+      },
+      {
+        name: "d4",
+        location: ["50rpx", "2rpx"],
+        handlerName: "d4Click"
+      },
+      {
+        name: "w6",
+        location: ["60rpx", "2rpx"],
+        handlerName: "w6Click"
       }
+    ],
+    buttons: [{
+      type: "default",
+      className: "",
+      text: "I'll use anyway",
+      value: 0
+    },
+    {
+      type: "primary",
+      className: "",
+      text: "Sure, I see",
+      value: 1
+    }
     ]
   },
 
